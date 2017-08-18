ed66205a8d116a056d323efd9fb327a8d1b0725c
Crisciano S. Botelho <cris@apeppers.com.br>
2017-08-18 11:41:32 -0300

    components in componests

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,16 +1,56 @@
 <template>
-  <div class="index">
-    <h1>{{ msg }}</h1>
+  <div>
+    <section>
+      <div class="container">
+        <div class="row">
+          <h1>{{ msg }}</h1>
+
+          <div class="col s12 m6">
+            <div class="card">
+              <div class="card-image">
+                <img src="../assets/img/sample-1.jpg">
+                <span class="card-title">Card Title</span>
+                <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+              </div>
+              <div class="card-content">
+                <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col s12 m6">
+            <div class="card">
+              <div class="card-image">
+                <img src="../assets/img/sample-1.jpg">
+                <span class="card-title">Card Title</span>
+                <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+              </div>
+              <div class="card-content">
+                <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </section>
+    <!--  separador  -->
+    <separador></separador>
   </div>
 </template>
 
 <script>
+import Separador from '@/components/Separador'
+
 export default {
   name: 'index',
   data () {
     return {
       msg: 'index'
     }
+  },
+  components: {
+    Separador
   }
 }
 </script>
---
+++ src/components/Separador.vue
@@ -0,0 +1,34 @@
+<template>
+  <section id="separador">
+    <div class="container">
+      <div class="row">
+        <h1>{{ msg }}</h1>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+
+export default {
+  name: 'separador',
+  data () {
+    return {
+      msg: 'separador'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  #separador{
+    background: url(../assets/img/sample-1.jpg);
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-attachment: fixed; 
+  }
+  section{ padding: 80px 0  }
+  h1,h2,h3,h4,h5{ color: #fff; }
+</style>
